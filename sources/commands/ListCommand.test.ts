813b95dcac5aca73ba84ddb43a00adadb5b20fc4
강찬규 <kenneth.kang@toss.im>
2021-09-23 14:23:45 +0900

    remove beforeEach, afterEach

--- sources/commands/ListCommand.test.ts
+++ sources/commands/ListCommand.test.ts
@@ -1,108 +1,131 @@
-import { CommitResult } from 'simple-git/promise';
 import { initializeTestRepository, Package, Repository } from '../../testing/repository';
 
+async function setup() {
+  const repository = await initializeTestRepository();
+  const packages = await Promise.all([
+    repository.addPackage('package1'),
+    repository.addPackage('package2'),
+    repository.addPackage('package3'),
+  ]);
+  const initialCommit = await repository.commitAll('Add packages');
+  return {
+    repository,
+    packages,
+    initialCommit,
+  };
+}
+
 describe('ListCommand', () => {
-  let repository: Repository;
-  let packages: [Package, Package, Package];
-  let initialCommit: CommitResult;
-
-  beforeEach(async () => {
-    repository = await initializeTestRepository();
-    packages = await Promise.all([
-      repository.addPackage('package1'),
-      repository.addPackage('package2'),
-      repository.addPackage('package3'),
-    ]);
-    initialCommit = await repository.commitAll('Add packages');
-  });
-  afterEach(async () => {
-    await repository.cleanup();
-  });
-
   describe('yarn workspaces since list <from>', () => {
     it('모든 package에 변경사항이 없으면 아무 것도 출력하지 않는다', async () => {
-      await repository.commitAll('Empty commit');
+      const { repository, initialCommit } = await setup();
+      try {
+        await repository.commitAll('Empty commit');
 
-      const result = await repository.exec('yarn', [
-        'workspaces',
-        'since',
-        'list',
-        initialCommit.commit,
-      ]);
+        const result = await repository.exec('yarn', [
+          'workspaces',
+          'since',
+          'list',
+          initialCommit.commit,
+        ]);
 
-      expect(result.stdout).toBe('');
+        expect(result.stdout).toBe('');
+      } finally {
+        await repository.cleanup();
+      }
     });
     it('변경된 package가 있다면 해당 package들을 출력한다', async () => {
-      const [package1, package2] = packages;
+      const {
+        repository,
+        initialCommit,
+        packages: [package1, package2],
+      } = await setup();
 
-      await Promise.all([
-        package1.addFile('package1.ts', '"I am updated"'),
-        package2.addFile('package2.ts', '"I am updated"'),
-      ]);
+      try {
+        await Promise.all([
+          package1.addFile('package1.ts', '"I am updated"'),
+          package2.addFile('package2.ts', '"I am updated"'),
+        ]);
 
-      await repository.commitAll('Update package1 and package2');
+        await repository.commitAll('Update package1 and package2');
 
-      const result = await repository.exec('yarn', [
-        'workspaces',
-        'since',
-        'list',
-        initialCommit.commit,
-      ]);
+        const result = await repository.exec('yarn', [
+          'workspaces',
+          'since',
+          'list',
+          initialCommit.commit,
+        ]);
 
-      const updatedPackages = result.stdout.split('\n');
-      expect(updatedPackages).toContain('packages/package1');
-      expect(updatedPackages).toContain('packages/package2');
+        const updatedPackages = result.stdout.split('\n');
+        expect(updatedPackages).toContain('packages/package1');
+        expect(updatedPackages).toContain('packages/package2');
+      } finally {
+        await repository.cleanup();
+      }
     });
   });
 
   describe('yarn workspaces since list <from> <to>', () => {
-    let firstCommit: CommitResult;
-    let secondCommit: CommitResult;
-
-    beforeEach(async () => {
-      const [package1, package2, package3] = packages;
+    async function setupTestCommits() {
+      const setupResult = await setup();
+      const {
+        repository,
+        packages: [package1, package2, package3],
+      } = setupResult;
 
       await Promise.all([
         package1.addFile('package1.ts', '"I am updated"'),
         package2.addFile('package2.ts', '"I am updated"'),
       ]);
 
-      firstCommit = await repository.commitAll('Update package1 and package2');
+      const firstCommit = await repository.commitAll('Update package1 and package2');
 
       await Promise.all([
         package2.addFile('package2.tsx', '"I am updated"'),
         package3.addFile('package3.tsx', '"I am updated"'),
       ]);
 
-      secondCommit = await repository.commitAll('Update package2 and package3');
-    });
+      const secondCommit = await repository.commitAll('Update package2 and package3');
+
+      return { ...setupResult, firstCommit, secondCommit };
+    }
 
     it('<to>까지 변경된 package들을 출력한다 1', async () => {
-      const result = await repository.exec('yarn', [
-        'workspaces',
-        'since',
-        'list',
-        initialCommit.commit,
-        firstCommit.commit,
-      ]);
+      const { repository, initialCommit, firstCommit } = await setupTestCommits();
+      try {
+        const result = await repository.exec('yarn', [
+          'workspaces',
+          'since',
+          'list',
+          initialCommit.commit,
+          firstCommit.commit,
+        ]);
 
-      const updatedPackages = result.stdout.split('\n');
-      expect(updatedPackages).toContain('packages/package1');
-      expect(updatedPackages).toContain('packages/package2');
+        const updatedPackages = result.stdout.split('\n');
+        expect(updatedPackages).toContain('packages/package1');
+        expect(updatedPackages).toContain('packages/package2');
+      } finally {
+        await repository.cleanup();
+      }
     });
     it('<to>까지 변경된 package들을 출력한다 2', async () => {
-      const result = await repository.exec('yarn', [
-        'workspaces',
-        'since',
-        'list',
-        initialCommit.commit,
-        secondCommit.commit,
-      ]);
+      const { repository, initialCommit, secondCommit } = await setupTestCommits();
+      try {
+        const result = await repository.exec('yarn', [
+          'workspaces',
+          'since',
+          'list',
+          initialCommit.commit,
+          secondCommit.commit,
+        ]);
 
-      const updatedPackages = result.stdout.split('\n');
-      expect(updatedPackages).toContain('packages/package1');
-      expect(updatedPackages).toContain('packages/package2');
-      expect(updatedPackages).toContain('packages/package3');
+        const updatedPackages = result.stdout.split('\n');
+        expect(updatedPackages).toContain('packages/package1');
+        expect(updatedPackages).toContain('packages/package2');
+        expect(updatedPackages).toContain('packages/package3');
+      } finally {
+        await repository.cleanup();
+      }
     });
   });
 });
